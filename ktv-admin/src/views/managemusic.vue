5d051f63bb6ec9b7b3be32a84fcac1e14b12df36
allen-lhj <5524172+allen-abby@user.noreply.gitee.com>
2020-05-23 22:57:42 +0800

    music manage

---
+++ ktv-admin/src/views/managemusic.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="magnagemusic">
+        <!-- 音频 -->
+        <audio ref="musicPlayer" :src="toListenSrc" @ended="ended" autoplay preload="load"></audio>
+        <Row style="padding:20px 60px 20px 40px">
+            <i-col :span="5">
+                <i-input prefix="ios-musical-notes" clearable v-model="searchName" @keypress.enter.native="searchSong()">
+            </i-col>
+            <i-col :span="3">
+                <Button type="primary" style="margin-left:5px;" @click="getAllSong()">所有歌曲</Button>
+            </i-col>
+            <i-col :span="4">
+                <Button type="success" style="margin-left:5px;" @click="toListen = true">试听歌曲</Button>
+            </i-col>
+            <i-col :span="12" style="text-algin:right">
+                <Avatar id="musicPoster" @click.native="operaMusic" class="listen-poster" v-if="toListenPoster" :src="toListenPoster" size="large" style="margin-right:40px;" />
+                <Button type="primary" @click="showAddSongBtn()">添加</Button>
+            </i-col>
+        </Row>
+    </div>
+</template>
